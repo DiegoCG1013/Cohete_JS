6ae2a41609263025da3a757f221529fa471a1dbf
Diego Carrasco <113851722+DiegoCG1013@users.noreply.github.com>
2024-03-21 17:50:10 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let paises = ['Omán','Israel', 'Grecia', 'Italia','España','Portugal','O. Atlántico','O. Atlántico','O. Atlántico','O. Atlántico','O. Atlántico','O. Atlántico','Caribe','Panamá', 'México','O. Pacífico','O. Pacífico','O. Pacífico','O. Pacífico','O. Pacífico'];
+
+
+let combustible = 2;
+
+calcular(paises, combustible);
+
+
+function calcular (paises, combustible){
+  let step = 100/paises.length;
+  let avance = 0;
+  let distancia = combustible * step;
+  
+  for(let i = 0; i < paises.length; i++) {
+    if(avance < distancia) {
+      console.log(`Pasa por ${paises[i]} sin incidentes`);
+    }
+    else {
+      console.log(`BOOOOOM, adiós ${paises[i]}`);
+    break;
+    }
+    avance += step;
+  }
+}
